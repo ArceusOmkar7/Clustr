af8c87cf354470fffec0d3c1ec57303b01372ffa
ArceusOmkar7 <omkarmahindrakar24@gmail.com>
2025-05-25 19:14:50 +0530

    shifted blip container to a seperate git repo

--- blip_container/app/__init__.py
+++
@@ -1,4 +0,0 @@
-# App package initialization
-from .api import create_app
-
-__all__ = ["create_app"]
--- blip_container/app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Models package initialization
-from .schemas import CaptionRequest, CaptionResponse, BatchCaptionRequest, BatchCaptionResponse
-
-__all__ = [
-    "CaptionRequest",
-    "CaptionResponse",
-    "BatchCaptionRequest",
-    "BatchCaptionResponse"
-]
